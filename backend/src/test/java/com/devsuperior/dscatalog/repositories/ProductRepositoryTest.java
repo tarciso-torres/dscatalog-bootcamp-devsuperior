b6672d2d27cc0335b4cda3619b1622b340a22ce6
Tarciso Torres <tarcisoftorres@gmail.com>
2021-05-21 13:54:28 -0300

    test: ensure return empty product with id is not exist

--- backend/src/test/java/com/devsuperior/dscatalog/repositories/ProductRepositoryTest.java
+++ backend/src/test/java/com/devsuperior/dscatalog/repositories/ProductRepositoryTest.java
@@ -14,45 +14,60 @@ import com.devsuperior.dscatalog.tests.Factory;
 
 @DataJpaTest
 class ProductRepositoryTest {
-	
+
 	@Autowired
 	private ProductRepository repository;
-	
+
 	private Long existingId;
 	private Long nomExistingId;
 	private Long countTotalProducts;
-	
+	private Product product;
+
 	@BeforeEach
-	void setUp() throws Exception{
+	void setUp() throws Exception {
 		existingId = 1l;
 		nomExistingId = 1000l;
 		countTotalProducts = 25L;
+		product = Factory.createProduct();
 	}
-	
+
 	@Test
 	void shouldPersistWithAutoincrementWhenIdIsNull() {
-		Product product = Factory.createProduct();
 		product.setId(null);
-		
+
 		product = repository.save(product);
-		
+
 		Assertions.assertNotNull(product.getId());
 		Assertions.assertEquals(countTotalProducts + 1, product.getId());
 	}
-	
+
+	@Test
+	void shouldFindProductWithIdExist() {
+
+		Optional<Product> result = repository.findById(product.getId());
+		Assertions.assertTrue(result.isPresent());
+	}
+
+	@Test
+	void shouldReturnEmptyProductWithIdIsNotExist() {
+
+		Optional<Product> result = repository.findById(nomExistingId);
+		Assertions.assertTrue(result.isEmpty());
+	}
+
 	@Test
 	void shouldDeleteObjectWhenIdExists() {
-		
+
 		repository.deleteById(existingId);
-		
+
 		Optional<Product> result = repository.findById(existingId);
-		
+
 		Assertions.assertFalse(result.isPresent());
 	}
-	
+
 	@Test
 	void shouldThrowEmptyResultDataAccessExceptionWhenIdDoesNotExist() {
-		
+
 		Assertions.assertThrows(EmptyResultDataAccessException.class, () -> {
 			repository.deleteById(nomExistingId);
 		});
