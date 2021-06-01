418dc0ece36ec9278f114bda7c9eb107280e3806
Tarciso Torres <tarcisoftorres@gmail.com>
2021-06-01 15:53:43 -0300

    chore: create UserUpdateValidator

---
+++ backend/src/main/java/com/devsuperior/dscatalog/dto/UserUpdateDTO.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dscatalog.dto;
+
+import com.devsuperior.dscatalog.services.validation.UserUpdateValid;
+
+@UserUpdateValid
+public class UserUpdateDTO extends UserDTO {
+
+	private static final long serialVersionUID = 1L;
+
+	public UserUpdateDTO() {
+	}
+
+}
